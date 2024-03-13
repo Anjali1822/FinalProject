0e4b603cf35c024882da3ca1567f5630f5426246
Anjali Bhawari <anjalibhawari1810@gmail.com>
2024-03-13 12:06:30 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 // import Header from './components/Header/Header';
 // import Footer from './components/Footer/Footer';
-import LandingPage from './screens/LandingPage/LandingPage';
+
 import {BrowserRouter, Route,Routes} from 'react-router-dom'; //for routing purpose
 // import MyNotes from './screens/MyNotes/MyNotes';
 import RegisterScreen  from './screens/RegisterScreen/RegisterScreen';
